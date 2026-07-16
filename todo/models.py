f9dc94281cc33ac787e3042f4e170ca52acdaf04
1F10250090 <s1F102500900@iniad.org>
2026-07-16 11:19:13 +0900

    期限切れタスクの色変更

--- todo/models.py
+++ todo/models.py
@@ -9,7 +9,11 @@ class Task(models.Model):
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
 
-    def is_overdue(self, dt):
+    def is_overdue(self, dt=None):
         if self.due_at is None:
             return False
+        
+        if dt is None:
+            dt = timezone.now()
+            
         return self.due_at < dt
