8eae5f07875904cdb01261d81a9e3ff7df5475cd
1F10250225 <s1f102502250@iniad.org>
2026-07-16 11:27:03 +0900

    プロジェクト機能の追加

--- todo/models.py
+++ todo/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.utils import timezone
 
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
     posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True, related_name='tasks')
 
     def is_overdue(self, dt):
         if self.due_at is None:
